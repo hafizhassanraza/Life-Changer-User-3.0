f8bff014224d2174a85df83d9200ed3a13c71ed1
Hassan_Raza <hafizhassanraza.hh@gmail.com>
2024-04-13 21:27:21 +0500

    All Bug Fix - UserDetails

--- app/src/main/java/com/enfotrix/life_changer_user_2_0/ui/ActivityNominee.kt
+++ app/src/main/java/com/enfotrix/life_changer_user_2_0/ui/ActivityNominee.kt
@@ -49,6 +49,7 @@ class ActivityNominee : AppCompatActivity() {
         constants= Constants()
 
 
+        sharedPrefManager = SharedPrefManager(mContext)
 
 
 
@@ -112,8 +113,7 @@ class ActivityNominee : AppCompatActivity() {
                         if(jsonObject.getBoolean("success")==true){
 
 
-
-                            Toast.makeText(mContext, jsonObject.getString("message").toString(), Toast.LENGTH_SHORT).show()
+                            Toast.makeText(mContext, "Nominee Added!", Toast.LENGTH_SHORT).show()
 
                             if(intent.getStringExtra(constants.KEY_ACTIVITY_FLOW).equals(constants.VALUE_ACTIVITY_FLOW_USER_DETAILS)){
                                 isNomineeAdded=true
@@ -167,43 +167,13 @@ class ActivityNominee : AppCompatActivity() {
                 headers["Authorization"] = "Bearer ${sharedPrefManager.getToken()}" // Replace "token" with your actual token
                 return headers
             }
-            /*override fun getParams(): MutableMap<String, String> {
-                return hashMapOf(
-                    "name" to req.name,
-                    "cnic" to req.cnic,
-                    "pin" to req.pin,
-                    "address" to req.address,
-                    "phone" to req.phone,
-                    "father_name" to req.father_name
-                )
-            }*/
+
 
         }
 
 
         Volley.newRequestQueue(mContext).add(stringRequest)
 
-
-
-
-
-
-
-        /*lifecycleScope.launch {
-            nomineeViewModel.addNominee(nominee).observe(this@ActivityNominee) {
-                utils.endLoadingAnimation()
-                if (it == true) {
-                    sharedPrefManager.saveNominee(nominee)// id overwrite in repo
-                    Toast.makeText(mContext, constants.NOMINEE_SIGNUP_MESSAGE, Toast.LENGTH_SHORT).show()
-                    if(intent.getStringExtra(constants.KEY_ACTIVITY_FLOW).equals(constants.VALUE_ACTIVITY_FLOW_USER_DETAILS)){
-                        startActivity(Intent(mContext,ActivityUserDetails::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK))
-                        finish()
-                    }
-                }
-                else Toast.makeText(mContext, constants.SOMETHING_WENT_WRONG_MESSAGE, Toast.LENGTH_SHORT).show()
-
-            }
-        }*/
 
 
 
--- app/src/main/java/com/enfotrix/life_changer_user_2_0/ui/ActivityUserDetails.kt
+++ app/src/main/java/com/enfotrix/life_changer_user_2_0/ui/ActivityUserDetails.kt
@@ -144,6 +144,7 @@ class ActivityUserDetails : AppCompatActivity() {
         }*/
 
 
+        setData()
 
         binding.layAddNominee.setOnClickListener {
 
